07487de646042af57121403338e9e9b849d6e9f7
Nathan <45177569+nmstory@users.noreply.github.com>
2025-03-18 17:45:21 +0000

    Whitespace fix

--- src/client.cpp
+++ src/client.cpp
@@ -10,28 +10,28 @@ Client::~Client() {
 
 // Factory function to inject correct network session dependency, based on platform
 extern std::unique_ptr<SessionInterface> CreateSession() {
-    #ifdef _WIN32
-        return std::make_unique<WindowsSession>();
-    #elif __linux__
-        return std::make_unique<LinuxSession>();
-    #else
-        throw std::runtime_error("Unsupported platform");
-    #endif
+	#ifdef _WIN32
+		return std::make_unique<WindowsSession>();
+	#elif __linux__
+		return std::make_unique<LinuxSession>();
+	#else
+		throw std::runtime_error("Unsupported platform");
+	#endif
 }
 
 bool Client::init(int port) {
 	session = CreateSession();
 
-    session->init(port);
+	session->init(port);
 
-    return true;
+	return true;
 }
 
 bool Client::update(){
-    
-    session->update();
+	
+	session->update();
 
-    return true;
+	return true;
 }
 
 
