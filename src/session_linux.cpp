a908b7e8c0d6ae13e21a1c551f8dd3dc8f6e296a
Nathan <45177569+nmstory@users.noreply.github.com>
2025-04-25 17:57:35 +0100

    Squashed commit of the following:
    commit 36526fb4b7e4b8cdb668b0c7331f9a490ec8fd5d
    Merge: f3458d3 8e6a0a2
    Author: Nathan <45177569+nmstory@users.noreply.github.com>
    Date:   Tue Mar 25 01:38:40 2025 +0000
    
        Merge pull request #12 from nmstory/second-indentation-fix
    
        Second indentation fix
    
    commit 8e6a0a21f1a314b7bf15c89a69e87faa36372cb5
    Author: Nathan <45177569+nmstory@users.noreply.github.com>
    Date:   Tue Mar 25 01:37:44 2025 +0000
    
        Second indentation fix
    
    commit f3458d302b772345af83677110078b5fdae0f6fa
    Merge: 2935292 6536b09
    Author: Nathan <45177569+nmstory@users.noreply.github.com>
    Date:   Tue Mar 25 01:29:06 2025 +0000
    
        Merge pull request #11 from nmstory/indentation-fix
    
        Indentation fix
    
    commit 6536b09157069a6e788d088a1887373de9ef3519
    Author: Nathan <45177569+nmstory@users.noreply.github.com>
    Date:   Tue Mar 25 01:27:37 2025 +0000
    
        Indentation fix

--- src/session_linux.cpp
+++ src/session_linux.cpp
@@ -94,7 +94,7 @@ bool LinuxSession::update() {
 		std::string_view received_str(reinterpret_cast<const char*>(data.data()), data.size());
 		std::cout << "Received data: " << received_str << std::endl;
 
-        // Processing data received
+		// Processing data received
 		if (received_str == "PING") {
 			std::string pongMessage = "PONG";
 			sendto(sockFD, pongMessage.c_str(), pongMessage.length(), 0, (struct sockaddr*)&addr, sizeof(addr));
@@ -103,7 +103,7 @@ bool LinuxSession::update() {
 		else if (received_str == "PING") {
 			peers->push_back(Peer(addr));
 		}
-    }
+	}
 
 	return true;
 }
--- src/session_windows.cpp
+++ src/session_windows.cpp
@@ -110,7 +110,7 @@ bool WindowsSession::update() {
 		std::string_view received_str(reinterpret_cast<const char*>(data.data()), data.size());
 		std::cout << "Received data: " << received_str << std::endl;
 
-        // Processing data received
+		// Processing data received
 		if (received_str == "PING") {
 			std::string pongMessage = "PONG";
 			sendto(socket, pongMessage.c_str(), pongMessage.length(), 0, (struct sockaddr*)&addr, sizeof(addr));
@@ -119,8 +119,8 @@ bool WindowsSession::update() {
 		else if (received_str == "PING") {
 			peers->push_back(Peer(addr));
 		}
-    }
-
+	}
+	
 	return true;
 }
 
