5281088df5e8336c867a0354ee34009223c5adfb
Nathan <45177569+nmstory@users.noreply.github.com>
2026-05-05 18:58:00 +0100

    Create new demo

---
+++ examples/position-replication/main.cpp
@@ -0,0 +1,22 @@
+#pragma comment(lib, "ws2_32.lib")
+
+#include <client.h>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		std::puts("Insufficient number of parameters entered. Please define port number.");
+		return 1;
+	}
+
+	Client client;
+	client.init(std::stoi(argv[1]));
+
+	while (true) {
+		if (!client.update()) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
