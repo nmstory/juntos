036de82abf52f5c5ce28537be50b71a9e151835f
Nathan <45177569+nmstory@users.noreply.github.com>
2026-05-20 21:19:14 +0100

    receiver id fix

--- examples/position-replication/position.h
+++ examples/position-replication/position.h
@@ -5,9 +5,6 @@
 #include <iostream>
 
 struct PositionReplicable : public Replicable {
-	float x = 0.0f;
-	float y = 0.0f;
-
 	std::optional<std::vector<uint8_t>> serialize() override {
 		std::vector<uint8_t> buf(POSITION_PACKET_SIZE);
 		std::memcpy(buf.data(), &id, sizeof(int));
@@ -18,11 +15,15 @@ struct PositionReplicable : public Replicable {
 
 	void deserialize(const uint8_t* data, size_t len) override {
 		if (len < POSITION_PACKET_SIZE) return;
-		std::memcpy(&id, data, sizeof(int));
+		uint8_t recv_id;
+		std::memcpy(&recv_id, data, sizeof(int));
 		std::memcpy(&x, data + sizeof(int), sizeof(float));
 		std::memcpy(&y, data + sizeof(int) + sizeof(float), sizeof(float));
-		std::cout << "Received from client " << getClientID(id) << " position id: " << getLocalID(id) << " at: (" << x << ", " << y << ")\n";
+		std::cout << "Received from client " << getClientID(recv_id) << " position id: " << getObjectID(recv_id) << " at: (" << x << ", " << y << ")\n";
 	}
+
+	float x = 0.0f;
+	float y = 0.0f;
 
 	const int POSITION_PACKET_SIZE = sizeof(int) + (sizeof(float) * 2);
 };
