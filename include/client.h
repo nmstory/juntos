ead1a290dafc26302ccb018ac3b7b3a6b880fc96
Nathan <45177569+nmstory@users.noreply.github.com>
2026-06-04 21:24:58 +0100

    resolve conflict

--- include/client.h
+++ include/client.h
@@ -4,15 +4,8 @@
 
 #include <chrono>
 #include <memory>
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-#include <replicable.h>
-=======
 #include <optional>
 #include <string>
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 #include <session_interface.h>
 
 class Client {
@@ -20,24 +13,12 @@ public:
 	Client();
 	~Client();
 
-<<<<<<< Updated upstream
-	bool init(const int port);
-	bool send(const uint8_t* data, size_t len);
-	std::optional<std::vector<uint8_t>> update();
-=======
-<<<<<<< Updated upstream
-	bool init(int port);
-	bool init(int clientID, int port, Replicable* replicable);
-	bool update();
-=======
 	bool init(const int port);
 	bool init(const std::string& hostname, const int port,
 			std::optional<std::chrono::milliseconds> recvTimeout = std::nullopt);
 	bool addPeer(const std::string& hostname, const int port);
 	bool send(const uint8_t* data, size_t len);
 	std::optional<std::vector<uint8_t>> update();
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 private:
 	std::unique_ptr<SessionInterface> session;
 };
